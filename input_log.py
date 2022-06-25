fc958ce34c100b1b37c2d71ebdf3cd82ac3003c4
joshwhittick <xk18811@bristol.ac.uk>
2022-06-25 11:58:38 +0100

    Update input_log.py

--- input_log.py
+++ input_log.py
@@ -1,31 +1,44 @@
 lifts = []
 
-while True:
+lift_values = {
+"a" : "", #lift
+"b" : "", #date
+"c" : 0, #load
+"d" : 0, #sets
+"e" : 0, #reps
+"f" : 0, #sets * reps
+"g" : 0 #set * reps * load
+}
+
+def write_to_log(lift, date, lift_load, lift_sets, lift_reps, total_reps, total_load):
+    
+    line = f"{lift} on {date} with {lift_load} kgs sets: {lift_sets} reps: {lift_reps} total reps: {total_reps} total load: {total_load:g}"
+    
+    with open('lifting_log.txt', 'a') as f:
+        f.write('\n')
+        f.writelines(''.join(line))
+        
+while True: 
     print("Do you want to enter a new days lifting?")
     n = input("y/n:")
 
-    if n.strip() == "y":
-        date = input("enter date: ")
-        lifts.append(input("Enter exercise: ")) 
+    if n == "y":
+        lift_values["b"] = input("Enter date: ")    
+        lifts.append(input("Enter exercise: "))
 
-        while input("Do you want to enter a new exercise? (y/n)") == "y":
+        while input("Do you want to enter a new exercise? (y/n):") == "y":
             inpt = input("Enter another exercise:")
             lifts.append(inpt) 
 
         for lift in lifts:
-            print("enter values for", lift, "(whole kilos):")
-            lift_sets = input("sets:")
-            lift_reps = input("reps:")
-            lift_load = input("load:")
-            total_reps = int(lift_sets) * int(lift_reps)
-            total_load = int(total_reps) * float(lift_load)        
+            lift_values["a"] = lift
+            lift_values["c"] = float(input(f"Enter load for {lift_values['a']}:"))
+            lift_values["d"] = int(input(f"Enter sets for {lift_values['a']}:"))
+            lift_values["e"] = int(input(f"Enter reps for {lift_values['a']}:"))
+            lift_values['f'] = int(lift_values['d'] * lift_values['e'])
+            lift_values['g'] = int(lift_values['f'] * lift_values['c'])
 
-            more_lines = f"{lift} on {date} with {lift_load} kgs sets: {lift_sets} reps: {lift_reps} total reps: {total_reps} total load: {total_load:g}"
+            write_to_log(lift_values["a"], lift_values["b"], lift_values["c"], lift_values["d"], lift_values["e"], lift_values["f"], lift_values["g"])
 
-            with open('lifting_log.txt', 'a') as f:
-                f.write('\n')
-                f.writelines(''.join(more_lines))
-        lifts.clear()
-
-    elif n.strip() == "n":
+    elif n == "n":
         quit()
